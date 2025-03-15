d7f651ce352473589178d83579f4156ffe923c1d
Slava <aha1abu12@gmail.com>
2025-03-15 21:23:25 +0300

    code

---
+++ task1/laplace.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def solve(a, b, c, f):
+    n = len(b)
+    x = np.zeros(n)
+    alpha = np.zeros(n)
+    beta = np.zeros(n)
+
+    alpha[0] = -c[0] / b[0]
+    beta[0] = f[0] / b[0]
+
+    for i in range(1, n - 1):
+        denominator = b[i] + a[i - 1] * alpha[i - 1]
+        alpha[i] = -c[i] / denominator
+        beta[i] = (f[i] - a[i - 1] * beta[i - 1]) / denominator
+
+    x[-1] = (f[-1] - a[-1] * beta[-2]) / (b[-1] + a[-1] * alpha[-2])
+
+    for i in range(n - 2, -1, -1):
+        x[i] = alpha[i] * x[i + 1] + beta[i]
+
+    return np.array(x)
+
+def u_(x):
+    return np.sin(5 * x) + np.cos(3 * x)
+
+def f_(x):
+    return 25 * np.sin(5 * x) + 9 * np.cos(3 * x)
+#def f_(x):
+ #   return 1
---
+++ task1/main.py
@@ -0,0 +1,39 @@
+from laplace import *
+import matplotlib.pyplot as plt
+
+N = 2
+y = [4, 10, 20, 50, 100, 250, 500, 1000, 10000]
+L2_error = np.zeros(len(y))
+C_error = np.zeros(len(y))
+j = 0
+for N in y:
+    h = 1 / N
+    a = np.full(N - 2, -1)
+    b = np.full(N - 1, 2)
+    c = np.full(N - 2, -1)
+
+    x = np.linspace(0, 1, N + 1)
+    f = np.array([f_(x[i]) for i in range(1, N)]) * h**2
+
+    u0, uN = u_(0), u_(1)
+    f[0] += u0
+    f[-1] += uN
+
+    u = np.array([u_(x[i]) for i in range(1, N)])
+    solution = solve(a, b, c, f)
+    error = np.abs(solution - u)
+    L2_error[j] = np.linalg.norm(error)
+    C_error[j] = np.max(error)
+    j += 1
+
+print(*C_error, *L2_error)
+plt.figure(figsize=(12, 6))
+plt.plot(y, L2_error, marker='o', color='forestgreen', label='L2-norm')
+plt.plot(y, C_error, marker='o', color='y', label='Max-norm')
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('N')
+plt.ylabel('error (L2-norm)')
+plt.grid(True)
+plt.show()
